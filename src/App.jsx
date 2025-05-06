3716fc67075ab941332836072dce0ae52dab3e5f
Roger <rogersosayafc@gmail.com>
2025-05-06 01:26:20 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className="bg-red-500 min-h-screen
+    ">
+      asass Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam dignissimos maxime ex asperiores eius, quisquam laudantium vero repellendus unde eos deserunt voluptas sapiente recusandae corrupti minus molestias aliquid vitae id. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere assumenda quas soluta deleniti et illo pariatur dolor, cum qui explicabo. Iusto provident est ducimus laudantium animi at delectus! Voluptate, eveniet! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam aperiam sequi, inventore fugiat consectetur omnis, sit eaque fugit minima modi deserunt! Asperiores debitis quae illo voluptate impedit eaque! Commodi, recusandae.
+    </div>
+  )
+}
+
+export default App
